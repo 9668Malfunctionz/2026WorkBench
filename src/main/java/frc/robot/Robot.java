06064b138690a71727266d0b8989ec56a7f20057
rrmcmurry <56413963+rrmcmurry@users.noreply.github.com>
2026-09-04 17:01:46 -0400

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Robot extends TimedRobot {
+  
+
+  private XboxController controller;
+
+  public static SparkMaxConfig DefaultConfig = new SparkMaxConfig();    
+  private SparkMax motor3; 
+  public static final int kmotorCanID = 2;
+
+  static {
+    DefaultConfig.smartCurrentLimit(50);
+    DefaultConfig.idleMode(IdleMode.kCoast);
+    DefaultConfig.openLoopRampRate(1.0);
+    DefaultConfig.inverted(false);
+  }
+
+
+  public Robot() {}
+
+
+  @Override
+  public void robotInit() {
+    motor3 = new SparkMax(2, MotorType.kBrushless);
+    motor3.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    controller = new XboxController(0);
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    
+    if (controller.getAButton()) {
+      motor3.set(-0.6);      
+    }
+    else {
+      motor3.stopMotor();
+    }
+
+ 
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
